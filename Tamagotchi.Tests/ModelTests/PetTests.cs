f89f20a48da9322a2be4495f5dd6563c0a9ce89f
Bess Campbell <bess.k.campbell@gmail.com>
2020-12-16 16:00:15 -0800

    Show test is written for time passing method
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- Tamagotchi.Tests/ModelTests/PetTests.cs
+++ Tamagotchi.Tests/ModelTests/PetTests.cs
@@ -22,9 +22,25 @@ namespace Tamagotchi.TestTools
       string name = "Brownie";
       Pet gigaPet = new Pet(name);
       //Act
-      string result = "Browni";
+      string result = gigaPet.Name;
       //Assert
-      Assert.AreEqual(gigaPet.Name, result);
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void Time_ReturnReducedFoodAttentionRest_int()
+    {
+      //Arrange
+      Pet gigaPet2 = new Pet("Rowdy");
+      //Act
+      Pet result = gigaPet2.Time();
+      int food = result.Food;
+      int attention = result.Attention;
+      int rest = result.Rest;
+      //Assert
+      Assert.AreEqual(food, gigaPet2.Food);
+      Assert.AreEqual(attention, gigaPet2.Attention);
+      Assert.AreEqual(rest, gigaPet2.Rest);
     }
   }
 }
